72b90a980c77610875fdc0b8cb2fcefe9b77c081
Onur DOKER <odoker@msn.com>
2025-07-24 22:28:38 +0300

    main.js uploaded

--- src/main.js
+++ src/main.js
@@ -1,34 +1,34 @@
-import Render from './js/render-functions';
-import PixabayAPI from './js/pixabay-api';
+import Render from "./js/render-functions";
+import PixabayAPI from "./js/pixabay-api";
 
 const renderInst = new Render();
 const pixabayInst = new PixabayAPI();
 
-const searchImg = document.querySelector('.form-search');
+const searchImg = document.querySelector(".form-search");
 
-searchImg.addEventListener('submit', event => {
+searchImg.addEventListener("submit", event => {
   event.preventDefault();
-  const searchTerm = searchImg.elements['search_string'].value.trim();
+  const searchTerm = searchImg.elements["search_string"].value.trim();
   if (!searchTerm) {
-    renderInst.showErrorMsg('Empty field!');
+    renderInst.showErrorMsg("Empty field!");
     return;
   }
 
   renderInst.toggleLoadingMsg();
-  pixabayInst.searchImg(searchTerm).then(responce => {
+  pixabayInst.searchImg(searchTerm).then(response => {
     renderInst.toggleLoadingMsg();
-    renderInst.showGalery(responce.data, pixabayInst.page,  pixabayInst.per_page);
+    renderInst.showGalery(response.data, pixabayInst.page, pixabayInst.per_page);
   });
 });
 
-const loadButton = document.querySelector('button[type="button"]');
-loadButton.addEventListener('click', renderNextPage); 
+const loadButton = document.querySelector("button[type=\"button\"]");
+loadButton.addEventListener("click", renderNextPage);
 
 function renderNextPage() {
   renderInst.toggleLoadingMsg();
-  pixabayInst.searchImg().then(responce => {
+  pixabayInst.searchImg().then(response => {
     renderInst.toggleLoadingMsg();
-    renderInst.showGalery(responce.data, pixabayInst.page, pixabayInst.per_page)
+    renderInst.showGalery(response.data, pixabayInst.page, pixabayInst.per_page);
     renderInst.scrollGallery();
-  })
+  });
 }
