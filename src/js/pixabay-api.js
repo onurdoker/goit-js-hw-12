428f494491981bc17b17e2708f82c4a2a1407326
Onur DOKER <odoker@msn.com>
2025-07-24 12:55:24 +0300

    pixabay-api.js uploaded

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,8 +4,9 @@ const apiKey = "51289001-701d2fcdb5357aeffdf8b918c";
 const perPage = 15;
 
 export async function searchImages(query, page) {
-  const url = `\`https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(
-    query)}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
+  const url = `https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(
+    query,
+  )}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
 
   try {
     const response = await axios.get(url);
